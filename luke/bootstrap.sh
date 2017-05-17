4b3a53153f0de244b20d4971cab787549c1d2a28
tpagel <timo.pagel@fhunii-media.com>
2017-05-17 18:24:55 +0200

    change

--- luke/bootstrap.sh
+++ luke/bootstrap.sh
@@ -7,3 +7,6 @@ sudo yum-config-manager \
 sudo yum install -y docker-ce
 sudo yum install -y setools-console
 sudo systemctl restart docker
+sudo ifup eth1
+
+sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
